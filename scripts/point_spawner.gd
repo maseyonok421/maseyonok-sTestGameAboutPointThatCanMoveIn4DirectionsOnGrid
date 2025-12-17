b202b453367817bb42da0008357be2ddcb6e2ff0
maseyonok421 <maseyonok421@gmail.com>
2025-12-17 20:04:27 +0300

    + Basic logic for Point Spawner

---
+++ scripts/point_spawner.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var pointScene = preload("res://prefabs//point.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("Point Spawner Inicialization: OK")
+
+
+func _on_timer_timeout() -> void:
+	
+	var pointInstance = pointScene.instantiate()
+	pointInstance.position = Vector2(
+		Global.fullCellSize, Global.fullCellSize
+	)
+	
+	add_child(pointInstance)
