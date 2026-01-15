db2164d330f7c7fd4b75694c02be255140bd6bb5
maseyonok421 <maseyonok421@gmail.com>
2026-01-15 16:22:37 +0300

    [+] Menu Screen

---
+++ scripts/main_menu.gd
@@ -0,0 +1,42 @@
+extends Node2D
+
+var curLbl = 0
+var labels
+
+@export var gameScene = preload("res://scenes/game.tscn").instantiate()
+
+func _ready() -> void:
+	labels = [$newGame, $load, $exit]
+	print("Main Menu Inicizlization OK")
+
+func changeSceneTo(scn):
+	var tree = get_tree()
+	var curScene = tree.get_current_scene()
+	tree.get_root().add_child(scn)
+	tree.set_current_scene(scn)
+	tree.get_root().remove_child(curScene)
+
+func redrawLabels():
+	for lbl in labels:
+		lbl.modulate = Color(0.5, 0.5, 0.5)
+	labels[curLbl].modulate = Color(1, 1, 1)
+	
+
+func _process(delta: float) -> void:
+	
+	if Input.is_action_just_pressed("ui_up"):
+		curLbl -= 1
+		if curLbl < 0: curLbl = 0
+	if Input.is_action_just_pressed("ui_down"):
+		curLbl += 1
+		if curLbl > 2: curLbl = 2
+	if Input.is_action_just_pressed("ui_accept"):
+		if   curLbl == 0:
+			changeSceneTo(gameScene)
+		elif curLbl == 1:
+			changeSceneTo(gameScene)
+		else:
+			get_tree().quit()
+	
+	
+	redrawLabels()
